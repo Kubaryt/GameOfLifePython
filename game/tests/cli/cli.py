da48b1c6428b40c87c77d9535613723da2f3527d
Kubaryt <kubastarzyk6554@gmail.com>
2025-06-09 15:15:26 +0200

    feat: Created tui

--- game/tests/cli/cli.py
+++ game/tests/cli/cli.py
@@ -1,4 +1,5 @@
 from game.utils.map import Map
+from game.utils.tui import prepare_display
 
 
 def test_from_arg():
@@ -32,6 +33,17 @@ def test_from_arg_and_from_file_are_identical():
     game_map_from_arg.load_from_str("..... .### # ....# .")
 
     game_map_from_file = Map()
-    game_map_from_file.load_from_file("game/tests/test_map.txt")
+    game_map_from_file.load_from_file("game/tests/cli/test_map.txt")
 
     assert game_map_from_arg.map == game_map_from_file.map
+
+
+def test_tui():
+    game_map = Map()
+    game_map.load_from_file("game/tests/cli/test_map.txt")
+
+    display = prepare_display(game_map, state={"paused": False})
+
+    expected_display = open("game/tests/cli/expected_display.txt").read()
+
+    assert display == expected_display
---
+++ game/utils/tui.py
@@ -0,0 +1,71 @@
+import threading
+from copy import deepcopy
+from time import sleep
+
+from pynput import keyboard
+from rich.console import Console
+from rich.live import Live
+
+from game.utils.map import Map
+
+
+def prepare_display(game_map: Map, state: dict) -> str:
+    display_str = ""
+    display_str += (
+        f"Generation: {game_map.generation} Population: {game_map.population}"
+        f" Press (p/r/q) to pause/restart game/quit.\n"
+    )
+    if state["paused"]:
+        display_str += "Paused\n\n"
+    else:
+        display_str += "\n"
+
+    display_str += str(game_map)
+
+    return display_str
+
+
+def detect_key_input(state: dict, lock):
+    def on_press(key):
+        try:
+            if key.char == "p":
+                with lock:
+                    state["paused"] = not state["paused"]
+            elif key.char == "r":
+                with lock:
+                    state["restart"] = True
+            elif key.char == "q":
+                with lock:
+                    state["quit"] = True
+                return
+        except AttributeError:
+            pass
+
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
+
+
+def run(game_map: Map):
+    console = Console()
+    starting_map = deepcopy(game_map)
+    state = {"paused": False, "restart": False, "quit": False}
+    lock = threading.Lock()
+
+    key_listener = threading.Thread(target=detect_key_input, args=(state, lock), daemon=True)
+    key_listener.start()
+
+    with Live(prepare_display(game_map, state), refresh_per_second=5, console=console) as live:
+        while game_map.population > 0:
+            with lock:
+                if state["quit"]:
+                    return
+                if state["restart"]:
+                    game_map = deepcopy(starting_map)
+                    live.update(prepare_display(game_map, state))
+                    state["restart"] = False
+
+                if not state["paused"]:
+                    game_map.next_generation()
+
+                live.update(prepare_display(game_map, state))
+                sleep(0.1)
